5e06b7e2820c7acaac779690ba40ad81cc08e36c
Aman Gupta <amangupta954055@gmail.com>
2024-03-06 17:07:52 +0530

    made a responsive carousel

--- app/(roots)/_components/CryptoDetails/Sentiments.tsx
+++ app/(roots)/_components/CryptoDetails/Sentiments.tsx
@@ -2,9 +2,8 @@
 import Popover from "@/components/utils/popover";
 import { Info } from "lucide-react";
 import React, { useState } from "react";
-import { EmblaOptionsType } from 'embla-carousel'
+import { EmblaOptionsType } from "embla-carousel";
 import Carousel from "../carousel";
-
 
 const Sentiments = () => {
   const [active, setActive] = useState<boolean>(false);
@@ -24,18 +23,51 @@ const Sentiments = () => {
         <div className="w-full ">
           <div className="w-full flex  items-center relative">
             <h1 className="text-3xl text-muted-foreground ">Key Events</h1>
-            <Info className="w-5 h-5 ml-4 mr-2  " onMouseEnter={() => setKey(true)}
-          onMouseLeave={() => setKey(false)} />
+            <Info
+              className="w-5 h-5 ml-4 mr-2  "
+              onMouseEnter={() => setKey(true)}
+              onMouseLeave={() => setKey(false)}
+            />
             <Popover active={key} text="Hello Developer" num={44} />
           </div>
         </div>
         <div className="w-full flex flex-col">
-            <Carousel />
-            <div className="w-full">
-                <div className=""> </div>
-                <div></div>
-
+          <Carousel />
+          <div className="w-full mt-20 flex flex-col">
+            <div>
+              <h1 className="text-3xl tracking-wider font-bold">
+                Analyst Estimates
+              </h1>
             </div>
+            <div className="flex w-full  mt-4">
+              <div className="bg-green-300/50 w-40 h-40 p-20 flex justify-center items-center rounded-full">
+                <h1 className="text-4xl tracking-tighter text-green-700 relative">76 <span className="absolute top-0 -right-4 text-lg">%</span></h1>
+              </div>
+              <div className="flex w-full flex-col justify-center items-center gap-5  ml-10 p-2">
+                <div className="flex w-full justify-center items-center gap-4">
+                    <h1 className="text-xl text-slate-400 tracking-wider">Buy</h1>
+                    <div className="flex relative w-full  items-center gap-2">
+                        <div className="w-[76%] h-4 bg-green-500 rounded-md" />
+                        <h1>76%</h1>
+                    </div>
+                </div>
+                <div className="flex w-full justify-center items-center gap-4">
+                    <h1 className="text-xl text-slate-400 tracking-wider">Hold</h1>
+                    <div className="flex relative w-full  items-center gap-2">
+                        <div className="w-[8%] h-4 bg-gray-400 rounded-md" />
+                        <h1>8%</h1>
+                    </div>
+                </div>
+                <div className="flex w-full justify-center items-center gap-4">
+                    <h1 className="text-xl text-slate-400 tracking-wider">Sell</h1>
+                    <div className="flex relative w-full  items-center gap-2">
+                        <div className="w-[16%] h-4 bg-red-500 rounded-md" />
+                        <h1>16%</h1>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- app/(roots)/_components/carousel.tsx
+++ app/(roots)/_components/carousel.tsx
@@ -43,28 +43,38 @@ export default function Carousel() {
   return (
     <div className="w-full h-[30vh] flex justify-center items-center mt-20">
       <Swiper
-        slidesPerView={"auto"}
+        slidesPerView={2}
         spaceBetween={80}
         navigation={true}
         modules={[Scrollbar, Navigation]}
         className="mySwiper"
+        breakpoints={{
+            0 : {
+                slidesPerView : 1,
+                spaceBetween : 20
+            },
+            768 : {
+                slidesPerView : 2,
+                spaceBetween : 80
+            },
+        }}
       >
         {data.map((el, i) => (
           <SwiperSlide key={i} className="" >
             <div
-              className={`flex ${el.bgColor} w-[35vw] bg-opacity-45 rounded-2xl p-4  h-full justify-center `}
+              className={`flex ${el.bgColor} md:w-[35vw] w-fit bg-opacity-45 rounded-2xl  p-4 lg:flex-row flex-col  h-full justify-center `}
             >
-              <div className={`logo  p-3  rounded-full mr-5 `}>
+              <div className={`logo  lg:p-3 w-fit rounded-full mr-1 lg:mr-5 `}>
                 <div className={`${el.iconColor} p-4 rounded-full `}>
 
                 <el.Icon className="w-6 h-6 font-bold text-white"  />
                 </div>
               </div>
               <div className="details w-full h-full">
-                <div className="mt-4 text-lg mb-5 ">
+                <div className="mt-4 text-[15px] lg:text-lg pb-1 lg:mb-5 ">
                   <h1>{el.title}</h1>
                 </div>
-                <div className="text-sm  tracking-wider">
+                <div className="lg:text-sm text-xs  tracking-wider">
                   <p>{el.desc}</p>
                 </div>
               </div>
